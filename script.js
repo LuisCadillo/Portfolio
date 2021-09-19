e943ced5aa7552133ff48361e0bcba19f8fb796e
Luis Cadillo <luiscaf3r@gmail.com>
2021-09-19 10:37:12 -0500

    New project added, custom scrollbar added, aboutme-section size changed

---
+++ script.js
@@ -0,0 +1,39 @@
+import OverlayScrollbars from 'OverlayScrollbars';
+import RevealFx from 'RevealFx'
+import './css/OverlayScrollbars.css'
+
+
+// Custom scrollbar
+const scrollbar = OverlayScrollbars(document.querySelector("body"), {
+      scrollbars: {
+        autoHide: "scroll",
+      },}); 
+
+const scrollToTopBtn = document.querySelector(".scrollToTopButton");
+const target = document.querySelector("footer");
+// Scroll to top
+(() => {
+  const callback = function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) scrollToTopBtn.classList.add("showed");
+      else scrollToTopBtn.classList.remove("showed");
+    });
+  };
+
+  const observer = new IntersectionObserver(callback);
+
+  observer.observe(target);
+
+  const scrollToTop = function () {
+       scrollbar.scroll({ y: "0" });
+    };
+  scrollToTopBtn.addEventListener("click", scrollToTop);
+})();
+
+// Reveal animations
+const myName = document.querySelector('h1');
+const nameRevealer = new RevealFx(myName,{});
+
+nameRevealer.reveal({
+  
+})
